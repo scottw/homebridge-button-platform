db0e64d899e73dff1a488785afd7fe5b54efa5d5
Scott Wiersdorf <scott@perlcode.org>
2025-09-29 21:53:49 -0600

    add eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: ['dist/**'],
+  },
+  {
+    rules: {
+      'quotes': ['warn', 'single'],
+      'indent': ['warn', 2, { 'SwitchCase': 1 }],
+      'semi': ['off'],
+      'comma-dangle': ['warn', 'always-multiline'],
+      'dot-notation': 'off',
+      'eqeqeq': 'warn',
+      'curly': ['warn', 'all'],
+      'brace-style': ['warn'],
+      'prefer-arrow-callback': ['warn'],
+      'max-len': ['warn', 140],
+      'no-console': ['warn'],
+      'comma-spacing': ['error'],
+      'no-multi-spaces': ['warn', { 'ignoreEOLComments': true }],
+      'no-trailing-spaces': ['warn'],
+      'lines-between-class-members': ['warn', 'always', { 'exceptAfterSingleLine': true }],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+    },
+  },
+);
